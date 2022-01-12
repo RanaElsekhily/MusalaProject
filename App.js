1c28e59b90f4d938d689a13767697b814e21863c
RanaElsekhily <ranaelsekhily@gmail.com>
2022-01-12 20:36:56 +0200

    feat: add new components / navigation to App.js

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Header } from 'react-native-elements';
 import NewsList from './components/NewsList';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import NewsDetails from './components/NewsDetails';
+
+
 
 class App extends Component {
   render() {
+    return <AppContainer />;
+
     return (
+      
       <View style={styles.container}>
         <Header
           placement="left"
@@ -19,6 +26,19 @@ class App extends Component {
     )
   }
 }
+
+
+const AppNavigator = createStackNavigator({
+  NewsList: {
+    screen: NewsList
+  },
+  NewsDetails: {
+    screen: NewsDetails
+  }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
 
 const styles = StyleSheet.create({
   container: {
