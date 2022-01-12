ffa224e415fe84d5cc912f708363280aab0e38f5
RanaElsekhily <ranaelsekhily@gmail.com>
2022-01-12 22:01:56 +0200

    fix: change App.js to add language and Tabs

--- App.js
+++ App.js
@@ -1,15 +1,71 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 import { Header } from 'react-native-elements';
 import NewsList from './components/NewsList';
+import Setting from './components/Settings';
+import './src/i18n';
+import { useTranslation } from 'react-i18next';
+
+
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import NewsDetails from './components/NewsDetails';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <AppContainer>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <NewsList/>
+      </View>
+    </AppContainer>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <AppContainer>
+
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <NewsList />
+      </View>
+    </AppContainer>
+  );
+}
 
 
+function MyTabs() {
+  const { t, i18n } = useTranslation();
+
+  const [currentLanguage, setLanguage] = useState('en');
+
+  const changeLanguage = value => {
+    i18n
+      .changeLanguage(value)
+      .then(() => setLanguage(value))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name={t('news')} component={HomeScreen} />
+      <Tab.Screen name={t('settings')} component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 class App extends Component {
+  
   render() {
-    return <AppContainer />;
+
+   
+    
+    return (<NavigationContainer>
+      <MyTabs/>
+    </NavigationContainer>
+    );
 
     return (
       
